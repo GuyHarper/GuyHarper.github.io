ff5dbbf4ee89054d391ea95e62a65e6d843143cb
= <guy.harper@gmail.com>
2018-01-23 00:03:54 +0000

    Building portfolio and contact pages

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(() => {
+
+  const $navbar = $('nav');
+  const $navbarBumper = $('.navbar-bumper');
+
+  const viewportHeight = $(window).height();
+  const navbarHeight = $navbar.height();
+  const scrollTarget = viewportHeight - navbarHeight;
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > scrollTarget) {
+      $navbar.addClass('fixed');
+      $navbarBumper.show();
+    } else if ($(window).scrollTop() <= scrollTarget) {
+      $navbar.removeClass('fixed');
+      $navbarBumper.hide();
+    }
+  });
+
+  $('a').click(function(e) {
+    const href = $(e.target).attr('href');
+    $('html, body').animate({
+      scrollTop: $(`${href}`).offset().top - $navbar.height()
+    }, 1000);
+  });
+
+
+});
